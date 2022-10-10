c0e9b9045560c301483a7e2b852289e2ad283ca5
Christian Tarello <christian_tarello@outlook.com>
2022-10-09 22:30:12 -0500

    Add number and operator logic

--- main.js
+++ main.js
@@ -26,3 +26,60 @@ function operate(operator, a, b) {
             return divide(a,b);
     }
 }
+
+
+let displayNumber = ""; //string
+let operator; //string
+let freshOperator; //boolean
+let answer ="a";
+
+
+function typeNumber(e) {
+    const typedNumber = e.target.value;
+
+    if (operator) {
+        if (freshOperator){
+            answer = displayNumber;
+            displayNumber = typedNumber;
+            display.textContent = `${displayNumber}`;
+            freshOperator = false;
+        } else {
+            displayNumber += typedNumber;
+            display.textContent = `${displayNumber}`;
+        }
+    } else {
+        if (answer) {
+            answer = 0;
+            displayNumber = typedNumber;
+            display.textContent = `${displayNumber}`;
+        }
+        else {
+            displayNumber += typedNumber;
+            display.textContent = `${displayNumber}`;
+        }
+    }
+}
+
+function chooseOperator(e) {
+    const eventOperator = e.target.value;
+    if (operator) {
+        displayNumber = operate(operator, +answer, +displayNumber);
+        answer = displayNumber;
+        operator = eventOperator;
+        freshOperator = true;
+        display.textContent = `${displayNumber}`
+    } else{
+        answer = displayNumber;
+        operator = eventOperator;
+        freshOperator = true;
+    }
+}
+
+const display = document.querySelector(".calculator-display")
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach((number)=>{number.addEventListener("click",typeNumber)});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operator)=>{operator.addEventListener("click",chooseOperator)});
+
